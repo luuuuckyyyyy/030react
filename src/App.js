7a89e22672a7f91de4dd684ab69d1df00514cfa6
Maxim Makarov <=>
2024-04-18 19:00:41 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,30 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="container">
+    <div className="col-3">
+      <div className="nav flex-column nav-pills">
+        <a href ="main" className='nav-link active' > Главаня</a>
+        <a href ="profile" className='nav-link'> Профиль</a>
+        <a href ="friend" className='nav-link'> Друзья</a>
+        <a href ="shop" className='nav-link'> Магазин</a>
+
+
+
+      </div>
+      <div className="col-9">
+
+      </div>
+    </div>
+    
+    </div>
+
+
+
+
+  );
+}
+
+export default App;
